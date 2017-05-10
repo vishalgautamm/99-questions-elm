dfa3c06ee5fb19e1de3e7fa2d9da91a21ff883a7
vishalgautamm <vishal.g@live.ca>
2017-05-10 03:02:54 -0400

    Update: Problem 6A - Palindrome String

--- problem6a.elm
+++ problem6a.elm
@@ -4,33 +4,17 @@
 module Main exposing (..)
 
 import String
-import List
 import Regex
 
 
--- filterAndReverse : String -> String
--- filterAndReverse str =
---     str
---         |> Regex.replace Regex.All (Regex.regex "[^a-zA-Z0-9]") (\_ -> "")
---         |> String.reverse
---         |> String.toUpper
---
---
--- isPalindromeStr : String -> Bool
--- isPalindromeStr str =
---     filterAndReverse str == String.toUpper str
+filterAndReverse : String -> String
+filterAndReverse str =
+    str
+        |> Regex.replace Regex.All (Regex.regex "[^a-zA-Z0-9]") (\_ -> "")
+        |> String.reverse
+        |> String.toUpper
 
 
-type Palindrome
-    = String
-    | List Char
-
-
-pally : Palindrome -> Bool
-pally xs =
-    case xs of
-        String ->
-            String.reverse xs == xs
-
-        List Char ->
-            List.reverse xs == xs
+isPalindromeStr : String -> Bool
+isPalindromeStr str =
+    filterAndReverse str == String.toUpper str
