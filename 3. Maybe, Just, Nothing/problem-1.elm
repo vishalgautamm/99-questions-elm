41d309b7c7e4dda62d7e6d57ef804bf7e1839c58
vishalgautamm <vishal.g@live.ca>
2017-06-12 10:35:59 -0400

    Part 3, Question 1

---
+++ 3. Maybe, Just, Nothing/problem-1.elm
@@ -0,0 +1,47 @@
+module Main exposing (..)
+
+import Html exposing (text)
+import Maybe
+
+
+last : List a -> Maybe a
+last xs =
+    case xs of
+        [] ->
+            Nothing
+
+        [ x ] ->
+            Just x
+
+        y :: ys ->
+            last xs
+
+
+main : Html.Html msg
+main =
+    Html.text
+        <| case test of
+            0 ->
+                "Your implementation passed all tests."
+
+            1 ->
+                "Your implementation failed one test."
+
+            x ->
+                "Your implementation failed " ++ (toString x) ++ " tests."
+
+
+test : Int
+test =
+    List.length
+        <| List.filter ((==) False)
+            [ last (1..4) == Just 4
+            , last [ 1 ] == Just 1
+            , last [] == Nothing
+            , last [ 'a', 'b', 'c' ] == Just 'c'
+            ]
+
+
+(..) : Int -> Int -> List Int
+(..) start end =
+    List.range start end
