0a3001ef0f2e0553b00b5054353f85cd82047893
vishalgautamm <vishal.g@live.ca>
2017-06-13 09:53:27 -0400

    Added Part 3

---
+++ 3. Maybe, Just, Nothing/problem-3.elm
@@ -0,0 +1,45 @@
+module Main exposing (..)
+
+import Html
+import List
+import Maybe
+
+
+-- Implement the function elementAt to return the n-th element of a list.
+-- The index is 1-relative, that is, the first element is at index 1.
+
+
+elementAt : List a -> Int -> Maybe a
+elementAt xs n =
+    case List.drop (n - 1) xs of
+        [] ->
+            Nothing
+
+        y :: ys ->
+            Just y
+
+
+main : Html.Html a
+main =
+    Html.text <|
+        case test of
+            0 ->
+                "Your implementation passed all tests."
+
+            1 ->
+                "Your implementation failed one test."
+
+            x ->
+                "Your implementation failed " ++ (toString x) ++ " tests."
+
+
+test : Int
+test =
+    List.length <|
+        List.filter ((==) False)
+            [ elementAt [ 1, 2, 3, 4 ] 2 == Just 2
+            , elementAt [ 1 ] 2 == Nothing
+            , elementAt [] 2 == Nothing
+            , elementAt [] (-1) == Nothing
+            , elementAt [ 'a', 'b', 'c' ] 2 == Just 'b'
+            ]
